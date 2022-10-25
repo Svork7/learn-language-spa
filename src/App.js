04bb8d6750d70566474a70d8f916254dc4cf2f38
Eugene Mikhailov <svork@ya.ru>
2022-10-25 21:24:14 +0300

    init spa language

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+class Cardes extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: 0,
+      word: '',
+      translate: '',
+      cards: [],
+    }
+  }
+};
+
+function updateInput(key, value) {
+  this.setState(state, {
+  [key]: value
+})}
+
+function addCard() {
+  const newCard = {
+    id: 1 + this.state.id,
+    value: {
+      word: this.state.word.slice(),
+      translate: this.state.translate.slice(),
+      turned: false
+    }
+  };
+  this.setState(state: {
+    id: newCard.id,
+    word: '',
+    translate: '',
+    cards: [...this.state.cards, newCard]
+  })
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="app-title">Карточки слов</h1>
+      <div className="container">
+        Добавить карточку
+        <br />
+        <input
+          type="text"
+          placeholder="введите слово"
+          value={this.state.word}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            this.updateInput('word', e.target.value)
+          }
+        />
+        <input
+          type="text"
+          placeholder="введите слово"
+          value={this.state.translate}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            this.updateInput('translate', e.target.value)
+          }
+        />
+        <button className='btn add-btn' onClick={() => this.addCard()}>Добавить слово</button>
+        <div>
+          {this.state.cards.map (card => {
+            return(
+              <div key={card.id}
+              className="card">
+                {card.value.turned ? card.value.translate : card.value.word}
+              </div>
+            )
+          }}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
