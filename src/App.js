a374140c1bb23d3cf883e289513fa41aa1710750
Eugene Mikhailov <svork@ya.ru>
2022-10-26 21:04:25 +0300

    language SPA done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './App.css'
 
-class Cardes extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {
       id: 0,
       word: '',
@@ -11,67 +12,82 @@ class Cardes extends React.Component {
       cards: [],
     }
   }
-};
 
-function updateInput(key, value) {
-  this.setState(state, {
-  [key]: value
-})}
+  updateInput(key, value) {
+    this.setState({
+      [key]: value,
+    })
+  }
 
-function addCard() {
-  const newCard = {
-    id: 1 + this.state.id,
-    value: {
-      word: this.state.word.slice(),
-      translate: this.state.translate.slice(),
-      turned: false
+  addCard() {
+    const newCard = {
+      id: 1 + this.state.id,
+      value: {
+        word: this.state.word.slice(),
+        translate: this.state.translate.slice(),
+        turned: false,
+      },
     }
-  };
-  this.setState(state: {
-    id: newCard.id,
-    word: '',
-    translate: '',
-    cards: [...this.state.cards, newCard]
-  })
-}
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="app-title">Карточки слов</h1>
-      <div className="container">
-        Добавить карточку
-        <br />
-        <input
-          type="text"
-          placeholder="введите слово"
-          value={this.state.word}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            this.updateInput('word', e.target.value)
-          }
-        />
-        <input
-          type="text"
-          placeholder="введите слово"
-          value={this.state.translate}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            this.updateInput('translate', e.target.value)
-          }
-        />
-        <button className='btn add-btn' onClick={() => this.addCard()}>Добавить слово</button>
-        <div>
-          {this.state.cards.map (card => {
-            return(
-              <div key={card.id}
-              className="card">
-                {card.value.turned ? card.value.translate : card.value.word}
-              </div>
-            )
-          }}
+    this.setState({
+      id: newCard.id,
+      word: '',
+      translate: '',
+      cards: [...this.state.cards, newCard],
+    })
+  }
+
+  turnCard(id) {
+    const cards = [...this.state.cards]
+
+    let index = cards.findIndex((card) => {
+      return card.id === id
+    })
+
+    cards[index].value.turned = !cards[index].value.turned
+
+    this.updateInput('cards', cards)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="app-title">Карточки слов</h1>
+        <div className="container">
+          Добавить карточку...
+          <br />
+          <input
+            type="text"
+            placeholder="Введите слово"
+            value={this.state.word}
+            onChange={(e) => this.updateInput('word', e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Введите перевод"
+            value={this.state.translate}
+            onChange={(e) => this.updateInput('translate', e.target.value)}
+          />
+          <button className="btn add-btn" onClick={() => this.addCard()}>
+            Добавить
+          </button>
+          <div>
+            {this.state.cards.map((card) => {
+              return (
+                <div
+                  key={card.id}
+                  className={'card' + (card.value.turned ? ' turned' : '')}
+                  onClick={() => this.turnCard(card.id)}
+                >
+                  {card.value.turned ? card.value.translate : card.value.word}
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
